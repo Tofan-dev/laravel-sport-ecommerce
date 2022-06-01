7b2ed8d2beea505e73b16214b81c7e92289ff185
Tofan <tofanalin1@gmail.com>
2022-06-01 12:26:10 +0300

    shop components

---
+++ resources/js/screens/public/ShopPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Info from "./components/Info";
+import Navbar from "./components/Navbar";
+import Slider from "./components/Slider";
+
+const ShopPage = () => {
+    return (
+        <>
+            <Info />
+            <Navbar />
+            <Slider />
+        </>
+    );
+};
+
+export default ShopPage;
---
+++ resources/js/screens/public/components/SliderData.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1615731446707-6ad24074bdd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        title: "Rachete",
+        description: "IEFTINE",
+        background: "f8uhdr",
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1615177981645-51107b202210?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        title: "Palete",
+        description: "IEFTINE SI BUNE",
+        background: "f53vbf",
+    },
+    {
+        id: 3,
+        img: "https://images.unsplash.com/photo-1559309106-ed14040fd35d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80",
+        title: "Rachete",
+        description: "IEFTINE",
+        background: "f5623f",
+    },
+];
